11b9f1356689f9fb939179278e559c1ea2f06792
Alexandru Scvortov <scvalex@gmail.com>
2011-06-02 23:18:52 +0100

    add login states to app

--- src/org/abstractbinary/BookTrader.java
+++ src/org/abstractbinary/BookTrader.java
@@ -6,10 +6,13 @@ import android.view.View;
 import android.widget.*;
 
 public class BookTrader extends Activity {
-    private static int STATE_NOT_LOGGED_IN = 0;
+    private final static int STATE_NOT_LOGGED_IN = 0;
+    private final static int STATE_LOGGING_IN = 1;
+    private final static int STATE_LOGGED_IN = 2;
     private int state;
 
     private FrameLayout menuBar;
+    private Button loginButton;
 
     /** Called when the activity is first created. */
     @Override
@@ -20,6 +23,8 @@ public class BookTrader extends Activity {
 
         menuBar = (FrameLayout)findViewById(R.id.menu_bar);
 
+        loginButton = (Button)findViewById(R.id.login_button);
+
         state = STATE_NOT_LOGGED_IN;
     }
 
@@ -29,7 +34,18 @@ public class BookTrader extends Activity {
     }
 
     private void nextState() {
-        Toast.makeText(this, "next state", Toast.LENGTH_SHORT).show();
+        if (state == STATE_NOT_LOGGED_IN) {
+            state = STATE_LOGGING_IN;
+            loginButton.setEnabled(false);
+        } else if (state == STATE_LOGGING_IN) {
+            state = STATE_LOGGED_IN;
+        } else if (state == STATE_LOGGED_IN) {
+            state = STATE_NOT_LOGGED_IN;
+            loginButton.setEnabled(true);
+        } else {
+            throw new RuntimeException("unknown state");
+        }
+
         View v = menuBar.getChildAt(menuBar.getChildCount() - 1);
         menuBar.removeView(v);
         menuBar.addView(v, 0);
