f09c849c09dbf897005523422cbf98d1f8a40d79
Alexandru Scvortov <scvalex@gmail.com>
2011-06-02 16:50:49 +0100

    add stub for menu-bar widget

--- src/org/abstractbinary/BookTrader.java
+++ src/org/abstractbinary/BookTrader.java
@@ -1,10 +1,17 @@
 package org.abstractbinary;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 
-public class BookTrader extends Activity
-{
+public class BookTrader extends Activity {
+    public class SpecialMenuBar extends View {
+        public SpecialMenuBar(Context context) {
+            super(context);
+        }
+    }
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
