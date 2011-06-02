8dce34de209add9a2887acb0096ace3ddecea880
Alexandru Scvortov <scvalex@gmail.com>
2011-06-02 18:17:38 +0100

    let's try again; the menubar is a FrameLayout

--- src/org/abstractbinary/BookTrader.java
+++ src/org/abstractbinary/BookTrader.java
@@ -4,11 +4,16 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class BookTrader extends Activity {
+    private static int STATE_NOT_LOGGED_IN = 0;
+    private int state;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        state = STATE_NOT_LOGGED_IN;
     }
 }
