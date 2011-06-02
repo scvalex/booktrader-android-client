dbd2cad6365080d3413638e99dd9b86a6ae78602
Alexandru Scvortov <scvalex@gmail.com>
2011-06-02 19:01:19 +0100

    meh finaly got the menu to alternate

--- src/org/abstractbinary/BookTrader.java
+++ src/org/abstractbinary/BookTrader.java
@@ -2,10 +2,14 @@ package org.abstractbinary;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 public class BookTrader extends Activity {
     private static int STATE_NOT_LOGGED_IN = 0;
     private int state;
+
+    private FrameLayout menuBar;
 
     /** Called when the activity is first created. */
     @Override
@@ -14,6 +18,20 @@ public class BookTrader extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        menuBar = (FrameLayout)findViewById(R.id.menu_bar);
+
         state = STATE_NOT_LOGGED_IN;
+    }
+
+    public void logIn(View v) {
+        Toast.makeText(this, "Log in", Toast.LENGTH_SHORT).show();
+        nextState();
+    }
+
+    private void nextState() {
+        Toast.makeText(this, "next state", Toast.LENGTH_SHORT).show();
+        View v = menuBar.getChildAt(menuBar.getChildCount() - 1);
+        menuBar.removeView(v);
+        menuBar.addView(v, 0);
     }
 }
