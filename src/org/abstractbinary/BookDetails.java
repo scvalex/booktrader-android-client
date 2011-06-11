77ecd6c1b0b417fe02472330fc3650bdc40403b8
Alexandru Scvortov <scvalex@gmail.com>
2011-06-11 17:48:33 +0100

    add book details activity

--- src/org/abstractbinary/BookDetails.java
+++ src/org/abstractbinary/BookDetails.java
@@ -1,6 +1,12 @@
 package org.abstractbinary.booktrader;
 
 import android.app.Activity;
+import android.os.Bundle;
 
-class BookDetails extends Activity {
+public class BookDetails extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.book_details);
+    }
 }
