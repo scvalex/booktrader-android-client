080dffe05ee975e10574fed2d2367f51f9fc7b1d
Alexandru Scvortov <scvalex@gmail.com>
2011-06-11 01:47:16 +0100

    more search results de-duplication

--- src/org/abstractbinary/SearchResult.java
+++ src/org/abstractbinary/SearchResult.java
@@ -5,7 +5,9 @@ import android.os.Handler;
 import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 
 public class SearchResult {
@@ -58,6 +60,9 @@ public class SearchResult {
     int totalItems;
     List<Book> books = new ArrayList<Book>();
 
+    /* Request de-duplication */
+    Set<Integer> alreadyGetting = new HashSet<Integer>();
+
 
     /* Public API */
 
@@ -73,9 +78,15 @@ public class SearchResult {
      * result, return null. */
     public synchronized Book get(int index, Handler handler) {
         if (index >= books.size()) {
-            Log.v(TAG, "getting more books... " + index);
-            BookTraderAPI.getInstance().moreSearchResults
-                (this, this.books.size(), handler);
+            int nextIndex = this.books.size();
+            if (!alreadyGetting.contains(nextIndex)) {
+                Log.v(TAG, "getting more books... " + index);
+                /* Temporary memory leak; only for the SearchResult's
+                 * lifetime, so I don't care. */
+                alreadyGetting.add(nextIndex);
+                BookTraderAPI.getInstance().moreSearchResults
+                    (this, nextIndex, handler);
+            }
             return FILLER_BOOK;
         }
         return books.get(index);
