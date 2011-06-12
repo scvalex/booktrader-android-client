033e3badaba661d8954c802a3c8bb5be371056b4
Alexandru Scvortov <scvalex@gmail.com>
2011-06-12 03:28:55 +0100

    add loading screen to book details page

--- src/org/abstractbinary/BookDetails.java
+++ src/org/abstractbinary/BookDetails.java
@@ -7,13 +7,20 @@ import android.os.Message;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+
 
 public class BookDetails extends Activity {
     /* Debugging */
     static final String TAG = "BookTrader";
 
+    /* Constants */
+    static final int DIALOG_LOADING = 0;
+
     /* Commonly used widgets */
     TextView bookTitleLabel;
+    ProgressDialog loadingDialog;
 
     /* Internal gubbins */
     String bookIdentifier;
@@ -39,12 +46,14 @@ public class BookDetails extends Activity {
                 public void handleMessage(Message msg) {
                     switch (msg.what) {
                     case BookTraderAPI.DETAILS_START:
-                        //whoosh
+                        showDialog(DIALOG_LOADING);
                         break;
                     case BookTraderAPI.DETAILS_GOT:
                         handleDetailsGot((SearchResult.Book)msg.obj);
                         break;
                     case BookTraderAPI.DETAILS_ERROR:
+                        if (loadingDialog != null)
+                            loadingDialog.dismiss();
                         Log.v(TAG, "Failed to get book details: " +
                               (Exception)msg.obj);
                         break;
@@ -61,10 +70,28 @@ public class BookDetails extends Activity {
         bookTitleLabel.setText(bookIdentifier);
     }
 
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        final Dialog dialog;
+        switch (id) {
+        case DIALOG_LOADING:
+            loadingDialog = new ProgressDialog(this);
+            dialog = loadingDialog;
+            loadingDialog.setMessage(getResources().getText(R.string.loading));
+            break;
+        default:
+            throw new RuntimeException("unknown dialog type + " + id);
+        }
+        return dialog;
+    }
+
 
     /* Event handlers */
 
     void handleDetailsGot(SearchResult.Book book) {
+        if (loadingDialog != null)
+            loadingDialog.dismiss();
+
         this.book = book;
         Toast.makeText(BookDetails.this, "details got", Toast.LENGTH_SHORT).show();
         bookTitleLabel.setText(book.title);
