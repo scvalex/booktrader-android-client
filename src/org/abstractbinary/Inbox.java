534b7587f4dbfa233d7ef30c28880abb277dd5a1
Alexandru Scvortov <scvalex@gmail.com>
2011-06-21 03:15:41 +0100

    some basic styling for inbox

--- src/org/abstractbinary/Inbox.java
+++ src/org/abstractbinary/Inbox.java
@@ -2,17 +2,25 @@ package org.abstractbinary.booktrader;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 
 public class Inbox extends ListActivity {
     /* Internal gubbins */
     MessagesAdapter messagesAdapter;
 
+    /* Common widgets */
+    TextView summaryText;
+
     /* Acitvity life-cycle */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.inbox);
+
+        summaryText = (TextView)findViewById(R.id.summary_text);
 
         messagesAdapter = new MessagesAdapter(this);
     }
